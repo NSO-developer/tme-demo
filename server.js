ad5d6c8653ad3abd0c641cdb64b7d1d33934a1ea
uforsgre <uforsgre@cisco.com>
2018-12-13 08:41:24 +0100

    Add keyboard shortcuts to dev webserver

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var webpack = require('webpack');
 var express = require('express');
 var config = require('./webpack.dev.js');
 var httpProxy = require('http-proxy');
+var readline = require('readline')
+var boxen = require('boxen');
 
 var app = express();
 var compiler = webpack(config);
@@ -44,4 +46,50 @@ app.listen(3000, function(err) {
   }
 
   console.log('Listening at http://localhost:3000/');
+  console.log();
 });
+
+/*
+ * Setup key events handling
+ */
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+
+process.stdin.on('keypress', (str, key) => {
+  if (!key.shift && key.ctrl && key.name === 'c') {
+    process.exit();
+  } else if (!key.shift && !key.ctrl && key.name === 'h') {
+    usage();
+  } else if (!key.shift && !key.ctrl && key.name === 'p') {
+    togglePayload();
+  } else {
+    console.log(`You pressed the "${str}" key`);
+    console.log();
+    console.log(key);
+    console.log();
+  }
+});
+
+var fPayload = false;
+function togglePayload() {
+    fPayload = !fPayload;
+    if (fPayload)
+        console.log("Payload printouts enabled");
+    else
+        console.log("Payload printouts disabled");
+}
+
+function usage() {
+  var usage_text = "NSO package UI hot update web server\n";
+  usage_text += "\n";
+  usage_text += "Key shortcuts:\n";
+  usage_text += "  h      - Show usage\n";
+  usage_text += "  p      - Enable jsonrpc payload printouts (to be impl.)\n";
+  usage_text += "  ctrl-c - Quit";
+  console.log(boxen(usage_text, {padding: 1, margin: 1, borderStyle: 'double'}));
+  console.log();
+}
+
+usage();
